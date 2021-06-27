3f6d105189dde038a8c6cba2b8f64b85ae43b754
SzymonTurek <szymko149@gmail.com>
2021-06-27 11:59:23 +0200

    Add a function to return the number of letters occurring more than once

---
+++ Zad2.py
@@ -0,0 +1,13 @@
+import collections
+
+def count_char (word):
+  c = collections.Counter(word.lower())
+  over_two = {x: count for x, count in c.items() if count >= 2}
+
+  key_list = [key for key in over_two.keys()]
+  print(len(over_two), tuple(key_list))
+  
+  return(len(over_two)) 
+
+print(count_char('aBcbA'))
+print(count_char('RabarbArka'))
